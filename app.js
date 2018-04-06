5f44ff9d0f85e98609e9fed2adfe0235e365d72a
cynthia <cynthiagviria@gmail.com>
2018-04-06 14:47:03 -0500

    cors 3.3

--- app.js
+++ app.js
@@ -19,10 +19,14 @@ app.use(cors());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+  if ('OPTIONS' == req.method) {
+     res.sendStatus(200);
+   }
+   else {
+     next();
+   }});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
