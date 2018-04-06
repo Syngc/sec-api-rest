63604841f3d808667e0804d32f8367ad1f5e61ac
cynthia <cynthiagviria@gmail.com>
2018-04-06 18:07:18 -0500

    body-parser

--- routes/dbconnection.js
+++ routes/dbconnection.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg')
 //var conString = 'postgress://cymajo:canoteo0987@secdb.czilpwlpevmw.us-east-2.rds.amazonaws.com:5432/SEC';
 
-
+/*
 const pool  = new Pool({
   host: 'secdb.czilpwlpevmw.us-east-2.rds.amazonaws.com',
   database: 'SEC',
@@ -11,8 +11,8 @@ const pool  = new Pool({
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
-});
-/*
+});*/
+
 
 const pool  = new Pool({
   host: 'ec2-54-163-240-54.compute-1.amazonaws.com',
@@ -24,6 +24,6 @@ const pool  = new Pool({
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
 });
-*/
+
 
 module.exports = pool;
