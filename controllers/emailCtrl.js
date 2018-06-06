451c9ba856fd57415e1560393f6ae3467d4a87c5
mateoc15 <teocanber1@gmail.com>
2018-06-06 00:07:57 -0500

    ll

--- controllers/emailCtrl.js
+++ controllers/emailCtrl.js
@@ -1,21 +1,22 @@
-var nodemailer = require('nodemailer'); 
+var nodemailer = require('nodemailer');
 
-module.exports = (email,user, pass) => {
+module.exports = (email, user, pass) => {
+    console.log("si entra aca");
     var transporter = nodemailer.createTransport({
         service: 'Gmail',
         auth: {
             user: 'noreplaysec1@gmail.com',
             pass: 'canoteo0987'
         }
-     });
+    });
     var mailOptions = {
         from: 'Remitente',
         to: email,
         subject: 'Bienvenido a SEC',
-        text: 'Bienvenido '+ user+ ' su contraseña es '+pass
+        text: 'Bienvenido ' + user + ' su contraseña es ' + pass
     };
-    transporter.sendMail(mailOptions, function(error, info){
-        if (error){
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
             console.log(error);
         } else {
             console.log("Email sent");
